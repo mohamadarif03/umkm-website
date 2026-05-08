ed7a13ed3a56d9cd8fda9477011dd6e7c9f2770b
afifalhauzan <afifalhauzan3@gmail.com>
2026-05-08 09:34:50 +0700

    fix: update app.jsx to app.tsx for TypeScript compatibility

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,11 +4,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     @viteReactRefresh  {{-- THIS MUST COME BEFORE @VITE --}}
-    @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
+    @vite(['resources/js/app.tsx', "resources/js/Pages/{$page['component']}.tsx"])
     @inertiaHead
 </head>
 <body class="antialiased">
     @inertia
 </body>
 </html>
-    
